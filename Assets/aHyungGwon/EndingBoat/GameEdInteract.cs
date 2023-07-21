90e70b7218bde362a3697f231b2bacc1eee55ad8
broccoli1962 <aq0011@naver.com>
2023-07-21 16:40:20 +0900

    Nice Boat Ending
    보트와 상호작용시 엔딩씬으로 넘어간다.

--- Assets/aHyungGwon/EndingBoat/GameEdInteract.cs
+++ Assets/aHyungGwon/EndingBoat/GameEdInteract.cs
@@ -1,18 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameEdInteract : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Animator animator;
+    Character character;
+    public GameObject player;
+    Rigidbody rigid;
+    
+    
     void Start()
     {
-        
-        }
+        animator = player.GetComponent<Animator>();
+        character = player.GetComponent<Character>();
+        rigid = player.GetComponent<Rigidbody>();
+    }
 
-        // Update is called once per frame
-        void Update()
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player"))
         {
-        
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                animator.SetBool("IsSitting", true);
+                player.transform.position = new Vector3(-10.5f, 3.5f, 29.6f);
+                player.transform.rotation = Quaternion.Euler(new Vector3(0,-90,0));
+                character.speed = 0;
+                SceneManager.LoadScene("EndingScene");
+            }
         }
+    }
 }
